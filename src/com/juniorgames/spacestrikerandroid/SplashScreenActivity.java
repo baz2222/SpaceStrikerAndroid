2ba890889a0886af27c3f8e99bb70177e3ddbc18
baz2222 <baz2222@i.ua>
2018-05-24 16:42:06 +0200

    LibGDX working without GRADLEgit add .!

--- src/com/juniorgames/spacestrikerandroid/SplashScreenActivity.java
+++ src/com/juniorgames/spacestrikerandroid/SplashScreenActivity.java
@@ -1,15 +1,16 @@
 package com.juniorgames.spacestrikerandroid;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-public class SplashScreenActivity extends Activity {
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AndroidApplication {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new SpaceStriker(),config);
     }
 }
